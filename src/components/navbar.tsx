fee5fb9b086812082c5561c707e8110c78ec7220
razikdontcare <gamersia4@gmail.com>
2024-12-19 20:28:23 +0800

    feat: enhance navbar and home layout with responsive design adjustments

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,23 +1,91 @@
+import { useState } from "react";
 import { NavLink } from "react-router";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      <nav className="w-full flex items-center justify-center bg-navbar z-40">
+      <div
+        className={`md:hidden flex-col flex items-center justify-center w-full absolute bg-white z-30 ${
+          open ? "top-20" : "-top-96"
+        } transition-all duration-500 ease-in-out p-5 gap-2`}
+      >
+        <NavLink
+          to={"/"}
+          className={({ isActive }) =>
+            isActive
+              ? "w-full bg-button text-center p-3 rounded-xl font-bold"
+              : "w-full text-center p-3"
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to={"/parser"}
+          className={({ isActive }) =>
+            isActive
+              ? "w-full bg-button text-center p-3 rounded-xl font-bold"
+              : "w-full text-center p-3"
+          }
+        >
+          Parser
+        </NavLink>
+      </div>
+      <nav className={`w-full flex items-center justify-center bg-navbar z-40`}>
         <div className="flex items-center justify-between container p-5 mx-auto">
-          <div className="flex items-center justify-center gap-5">
-            <div className="size-16 relative">
+          <div className="flex items-center justify-center gap-2 md:gap-5">
+            <div className="size-10 md:size-12 lg:size-16 relative">
               <img
                 src="/logo.png"
                 alt="4Parser"
                 className="absolute object-contain"
               />
             </div>
-            <span className="text-5xl font-bold italic text-white">
+            <span className="text-2xl md:text-3xl lg:text-5xl font-bold italic text-white">
               <span className="text-title">4</span>Parser
             </span>
           </div>
-          <div className="flex items-center justify-center gap-5 text-3xl text-white font-bold italic">
+          <div className="relative inset-0 md:hidden flex items-center justify-center gap-5 text-2xl lg:text-3xl text-white font-bold italic">
+            <button
+              onClick={() => setOpen((prev) => !prev)}
+              className="flex relative items-center justify-center px-3"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className={`size-8 ${
+                  open ? "opacity-0 rotate-45" : "opacity-100"
+                } absolute transition-all ease-in-out duration-300`}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 9h16.5m-16.5 6.75h16.5"
+                />
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className={`size-8 ${
+                  open ? "opacity-100" : "opacity-0 -rotate-45"
+                } absolute transition-all ease-in-out duration-300`}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="hidden md:flex items-center justify-center gap-5 text-2xl lg:text-3xl text-white font-bold italic">
             <NavLink to={"/"}>Home</NavLink>
             <NavLink to={"/parser"}>Parser</NavLink>
           </div>
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -12,18 +12,18 @@ function Home() {
         />
       </Helmet>
 
-      <div className="flex flex-col items-center mx-auto min-h-screen w-full bg-background">
+      <div className="flex flex-col items-center mx-auto min-h-screen w-full bg-background overflow-hidden">
         <Navbar />
 
-        <div className="flex items-center justify-center flex-col container mx-auto py-20 gap-24">
+        <div className="flex items-center justify-center flex-col container mx-auto py-20 gap-24 transition-all duration-300">
           <div className="flex items-center justify-center flex-col gap-3">
             <div className="flex flex-col gap-3 max-w-sm w-full items-center justify-center">
-              <h1 className="text-white text-4xl italic font-bold">
+              <h1 className="text-white text-2xl xl:text-4xl italic font-bold">
                 What is <span className="text-title">Parser</span>
               </h1>
               <span className="h-[0.1rem] w-full bg-title" />
             </div>
-            <p className="italic text-white text-center text-xl">
+            <p className="italic text-white md:text-center text-sm max-w-xs md:max-w-full text-justify xl:text-xl">
               Parser adalah alat atau program yang digunakan untuk menganalisis
               dan memproses data, seperti teks atau angka, dengan cara
               memecahnya menjadi bagian-bagian yang lebih kecil dan terstruktur.
@@ -34,12 +34,12 @@ function Home() {
           </div>
           <div className="flex items-center justify-center flex-col gap-3">
             <div className="flex flex-col gap-3 max-w-sm w-full items-center justify-center">
-              <h2 className="text-white text-4xl italic font-bold">
+              <h2 className="text-white text-2xl xl:text-4xl italic font-bold">
                 About <span className="text-title">Us</span>
               </h2>
               <span className="h-[0.1rem] w-full bg-title" />
             </div>
-            <p className="italic text-white text-center text-xl">
+            <p className="italic text-white md:text-center text-sm max-w-xs md:max-w-full text-justify xl:text-xl">
               4Parser adalah sebuah website yang dirancang untuk memproses dan
               menganalisis data dalam bentuk kalimat maupun angka. Website ini
               berfungsi untuk melakukan parsing, yaitu memecah data menjadi
@@ -50,31 +50,31 @@ function Home() {
           </div>
           <div className="flex items-center justify-center flex-col gap-3">
             <div className="flex flex-col gap-3 max-w-sm w-full items-center justify-center">
-              <h2 className="text-white text-4xl italic font-bold">
+              <h2 className="text-white text-2xl xl:text-4xl italic font-bold">
                 <span className="text-title">4</span>Parser Developer
               </h2>
               <span className="h-[0.1rem] w-full bg-title" />
             </div>
-            <div className="flex items-center justify-center container mx-auto gap-5 pt-5">
+            <div className="flex-col md:flex-row flex-nowrap flex md:flex-wrap items-center justify-center container mx-auto gap-5 pt-5 max-w-xs md:max-w-full">
               <img
                 src="/razik.jpg"
                 alt="razik"
-                className="aspect-square rounded-xl size-72"
+                className="aspect-square rounded-xl md:size-72 lg:size-52 xl:size-72"
               />
               <img
                 src="/rika.jpg"
                 alt="rika"
-                className="aspect-square rounded-xl size-72"
+                className="aspect-square rounded-xl md:size-72 lg:size-52 xl:size-72"
               />
               <img
                 src="/jonathan.jpg"
                 alt="jonathan"
-                className="aspect-square rounded-xl size-72"
+                className="aspect-square rounded-xl md:size-72 lg:size-52 xl:size-72"
               />
               <img
                 src="/krisna.jpg"
                 alt="krisna"
-                className="aspect-square rounded-xl size-72"
+                className="aspect-square rounded-xl md:size-72 lg:size-52 xl:size-72"
               />
             </div>
           </div>
